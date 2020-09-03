cad5b96ba734abeb95e43d1df9185e4a115666dd
Santoni Dyaz <tonisan1080p@gmail.com>
2020-09-03 21:28:23 +0700

    commit baru

--- app/src/main/java/com/example/projectbaruudahdiajaringerald/MainActivity.kt
+++ app/src/main/java/com/example/projectbaruudahdiajaringerald/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.projectbaruudahdiajaringerald
 
+
+
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun moveActivities(view: View) {
+        val button_Look = findViewById<Button>(R.id.buttonLook)
+        val intent = Intent(this, lookupList::class.java)
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/example/projectbaruudahdiajaringerald/lookupList.kt
@@ -0,0 +1,13 @@
+package com.example.projectbaruudahdiajaringerald
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lookupList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lookup_list)
+    }
+
+}
